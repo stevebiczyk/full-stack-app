4238cbade76f2bde6c11ac931b709389a3144086
stevebiczyk <stevebiczyk@gmail.com>
2024-06-12 13:44:18 +0100

    created Flask API with CRUD functions

---
+++ backend/config.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
